09032b4f7c4681332cb25a4349d62b274adf97d7
jin <jchj108@naver.com>
2022-08-24 21:55:06 +0900

    Initial commit

---
+++ src/main/java/com/example/dappchallenge/DAppChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dappchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DAppChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DAppChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dappchallenge/DAppChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dappchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAppChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
